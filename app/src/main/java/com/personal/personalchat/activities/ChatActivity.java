293fa533205ed69dd5009977f48f28334be29e00
VENTURA\ventu <abednego.bogichristian@ai.astra.co.id>
2023-06-04 18:08:01 +0700

    Chat Screen Layout Design

---
+++ app/src/main/java/com/personal/personalchat/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.personal.personalchat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.personal.personalchat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
