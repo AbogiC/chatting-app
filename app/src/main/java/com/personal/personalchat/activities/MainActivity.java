0a853d6b0ff2aaa17391443532a21b53af986d8f
VENTURA\ventu <abednego.bogichristian@ai.astra.co.id>
2023-06-04 15:38:29 +0700

    Sign Up Using Firestore Database

--- app/src/main/java/com/personal/personalchat/activities/MainActivity.java
+++ app/src/main/java/com/personal/personalchat/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.personal.personalchat;
+package com.personal.personalchat.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.personal.personalchat.R;
 
 public class MainActivity extends AppCompatActivity {
 
